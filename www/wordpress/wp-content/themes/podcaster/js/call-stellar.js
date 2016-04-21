b8fbef2551443da317e1231d93f311f2ed23133f
nob <yanbe.n@jointanet.com>
2016-04-21 13:55:36 +0900

    Initial commit with VCCW & Podcast plugin & Podcast theme

---
+++ www/wordpress/wp-content/themes/podcaster/js/call-stellar.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+	/* Call Stellar. js*/
+	$.stellar({
+		horizontalOffset: 0,
+		verticalOffset: 0,
+		horizontalScrolling: false,
+		responsive: true,
+		//parallaxBackgrounds: true,
+	});
+});
